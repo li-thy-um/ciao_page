90d20f488d1c9af51393cd04950ba12290553150
wongsean <platokon@gmail.com>
2016-03-14 23:46:33 +0800

    update

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -4,4 +4,12 @@ class ArticlesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
 end
