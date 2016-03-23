bf237fff524e7f3eb84aef2b073fb5e9c3a7bb06
wongsean <platokon@gmail.com>
2016-03-23 20:44:43 +0800

    static page test

--- test/controllers/static_page_controller_test.rb
+++ test/controllers/static_page_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPageControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select 'title', 'Ciao.moe'
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select 'title', 'About me - Ciao.moe'
   end
 
   test "should get admin" do
