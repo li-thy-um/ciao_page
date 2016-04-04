bdc2b788bb1231931ded70af0cd1a668d22f5bef
wongsean <platokon@gmail.com>
2016-04-04 18:10:26 +0800

    rspec for article model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	before(:each) do
+	  @article = Article.create(title: 'title',
+	  											 		author: 'platokon',
+	  											 		content: <<-EOF.gsub(/^\s+|/, ''))
+	  											 			|##title
+	  											 			|here is some content 
+	  											 		EOF
+	end
+
+	it 'is valid with correct content' do
+		expect(@article.valid?).to be_truthy
+	end
+
+	it 'is invalid with blank title' do
+		@article.title = ' ' * 10
+		expect(@article.valid?).to be_falsey
+	end
+
+	it 'should contains script tag' do
+		@article.content += "\n<script>alert(0)</script>"
+		@article.save
+		expect(@article.content_html).not_to match(/<script>|<\/script>/)
+	end
 end
