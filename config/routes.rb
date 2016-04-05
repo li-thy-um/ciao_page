8194d752de7af003edef1b24c085b5409c52631f
wongsean <platokon@gmail.com>
2016-04-05 15:59:15 +0800

    routes change

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,11 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  get 'admin/login' => 'session#new'
-  get 'admin/logout' => 'session#destroy'
-  post 'admin/login' => 'session#create'
-  get 'about' => 'static_page#about'
-  get 'admin' => 'static_page#admin'
+  get 'users/login' => 'session#new'
+  get 'users/logout' => 'session#destroy'
+  post 'users/login' => 'session#create'
+  get 'about' => 'home#about'
+  get 'admin' => 'home#admin'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
